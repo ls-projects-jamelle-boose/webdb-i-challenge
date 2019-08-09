f0e957e0f478abf7b8b75132e38d17633f699fcf
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2019-08-08 18:24:12 -0700

    add PUT, DELETE route

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -56,4 +56,56 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const count = await db("accounts")
+      .where("id", "=", id)
+      .update(data);
+    if (count) {
+      res.status(200).json({
+        success: true,
+        updated: count
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: `Could not find account ${id}.`
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `Server error, could not update account.`,
+      error: error
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await db("accounts")
+      .where("id", "=", id)
+      .del();
+
+    if (data) {
+      res.status(200).json({
+        success: true,
+        deleted: data
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: `Could not find post ${id}.`
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `Could not delete the account.`,
+      error
+    });
+  }
+});
+
 module.exports = router;
