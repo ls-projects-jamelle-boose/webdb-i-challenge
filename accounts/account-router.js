cbfd609590f22d77890ec6cc9dcda5bdc62e413b
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-10 17:33:11 -0700

    feat: add query option to GET /

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -1,109 +1,108 @@
-const express = require("express");
-
-//database access using knex
-const db = require("../data/dbConfig");
-
+const express = require('express');
+const db = require('../data/dbConfig');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
+  const { limit, orderby, sortdir } = req.query;
+  const queryLimit = limit || -1;
+  const queryOrderBy = orderby || 'id';
+  const querySortDir = sortdir || 'asc';
+
   try {
-    const accounts = await db("accounts");
-    console.log(accounts);
+    const accounts = await db('accounts')
+      .orderBy(queryOrderBy, querySortDir)
+      .limit(queryLimit);
     res.status(200).json({
       success: true,
-      accounts
+      accounts,
     });
   } catch (error) {
     res.status(500).json({
       message: `Error getting budget.`,
-      error
+      error,
     });
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const account = await db("accounts").where("id", id);
+    const account = await db('accounts').where('id', id);
     if (account[0] === undefined) {
       res.status(404).json({
         success: false,
-        message: `Account not found.`
+        message: `Account not found.`,
       });
     } else res.status(200).json(account);
   } catch (error) {
     res.status(500).json({
       success: false,
-      error
+      error,
     });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const data = req.body;
   try {
-    const newAccount = await db("accounts").insert(data);
+    const newAccount = await db('accounts').insert(data);
     res.status(201).json({
       success: true,
       newAccount,
-      data
+      data,
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      error
+      error,
     });
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const data = req.body;
   try {
-    const count = await db("accounts")
-      .where("id", "=", id)
-      .update(data);
+    const count = await db('accounts').where('id', '=', id).update(data);
     if (count) {
       res.status(200).json({
         success: true,
-        updated: count
+        updated: count,
       });
     } else {
       res.status(404).json({
         success: false,
-        message: `Could not find account ${id}.`
+        message: `Could not find account ${id}.`,
       });
     }
   } catch (error) {
     res.status(500).json({
       message: `Server error, could not update account.`,
-      error: error
+      error: error,
     });
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const data = await db("accounts")
-      .where("id", "=", id)
-      .del();
+    const data = await db('accounts').where('id', '=', id).del();
 
     if (data) {
       res.status(200).json({
         success: true,
-        deleted: data
+        deleted: data,
       });
     } else {
       res.status(404).json({
         success: false,
-        message: `Could not find post ${id}.`
+        message: `Could not find post ${id}.`,
       });
     }
   } catch (error) {
     res.status(500).json({
       message: `Could not delete the account.`,
-      error
+      error,
     });
   }
 });
